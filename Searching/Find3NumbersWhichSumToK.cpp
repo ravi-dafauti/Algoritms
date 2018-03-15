864f16ef744f5492a61fb80cc18c853b143cacba
ravinder dafauti <34746281+ravi-dafauti@users.noreply.github.com>
2018-03-15 16:58:02 +0530

    Find if 3 numbers sum to K in an array

---
+++ Searching/Find3NumbersWhichSumToK.cpp
@@ -0,0 +1,74 @@
+/*
+Write a program that, given an array A[] of n numbers and another number x,
+determines whether or not there exist three elements in S whose sum is exactly x.
+*/
+#include<iostream>
+using namespace std;
+
+void exchange(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int partition(int A[], int si, int ei)
+{
+	int x = A[ei];
+	int i = (si - 1);
+	int j;
+
+	for (j = si; j <= ei - 1; j++)
+	{
+		if (A[j] <= x)
+		{
+			i++;
+			exchange(&A[i], &A[j]);
+		}
+	}
+	exchange(&A[i + 1], &A[ei]);
+	return (i + 1);
+}
+
+void quickSort(int A[], int si, int ei)
+{
+	int pi;    /* Partitioning index */
+	if (si < ei)
+	{
+		pi = partition(A, si, ei);
+		quickSort(A, si, pi - 1);
+		quickSort(A, pi + 1, ei);
+	}
+}
+bool hasArrayThreeCandidates(int arr[], int n, int sum)
+{
+	quickSort(arr, 0, n - 1);
+	int i, j;
+	for (int k = 0; k < n - 2; k++)
+	{
+		for (i = k+1, j = n - 1; i < j;)
+		{
+			if (arr[k] + arr[i] + arr[j] == sum)
+				return true;
+			else if (arr[k] + arr[i] + arr[j] < sum)
+				i++;
+			else
+				j--;
+		}
+	}
+	return false;
+}
+int main()
+{
+	int A[] = { 1, 4, 45, 6, 10, -8 };
+	int n = 11;
+	int arr_size = 6;
+
+	if (hasArrayThreeCandidates(A, arr_size, n))
+		cout << "Array has three elements with given sum";
+	else
+		cout << "Array doesn't have three elements with given sum";
+
+	return 0;
+}
