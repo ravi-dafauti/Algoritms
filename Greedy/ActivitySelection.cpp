521844c45bb83dc7a785f5ce46f7da7c22bfa4aa
ravinder dafauti <34746281+ravi-dafauti@users.noreply.github.com>
2018-03-21 20:03:02 +0530

    Activity Selection

---
+++ Greedy/ActivitySelection.cpp
@@ -0,0 +1,59 @@
+/*
+Let us consider the Activity Selection problem as our first example of Greedy algorithms. Following is the problem statement.
+You are given n activities with their start and finish times. Select the maximum number of activities that can be performed by a single person, assuming that a person can only work on a single activity at a time.
+Example:
+
+Example 1 : Consider the following 3 activities sorted by
+by finish time.
+start[]  =  {10, 12, 20};
+finish[] =  {20, 25, 30};
+A person can perform at most two activities. The
+maximum set of activities that can be executed
+is {0, 2} [ These are indexes in start[] and
+finish[] ]
+
+Example 2 : Consider the following 6 activities
+sorted by by finish time.
+start[]  =  {1, 3, 0, 5, 8, 5};
+finish[] =  {2, 4, 6, 7, 9, 9};
+A person can perform at most four activities. The
+maximum set of activities that can be executed
+is {0, 1, 3, 4} [ These are indexes in start[] and
+finish[] ]
+*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+struct Activity
+{
+	int s;
+	int e;
+};
+bool compare(Activity a, Activity b)
+{
+	return a.e < b.e;
+}
+void printMaxActivities(Activity arr[], int n)
+{
+	sort(arr, arr + n, compare);
+	int lastindex = 0;
+	cout << "activities are : \n";
+	cout << "(" << arr[0].s << " , " << arr[0].e << ")  ";
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i].s >= arr[lastindex].e)
+		{
+			cout << "(" << arr[i].s << " , " << arr[i].e << ")  ";
+			lastindex = i;
+		}
+	}
+}
+int main()
+{
+	Activity arr[] = { { 5, 9 },{ 1, 2 },{ 3, 4 },{ 0, 6 },
+	{ 5, 7 },{ 8, 9 } };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	printMaxActivities(arr, n);
+	return 0;
+}
