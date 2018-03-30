b219b7f4b561ff8cd9ebbfea23953997f4ae847d
ravinder dafauti <34746281+ravi-dafauti@users.noreply.github.com>
2018-03-30 22:38:53 +0530

    Combinational Sum

--- Miscellaneous/CombinationalSum.cpp
+++ Miscellaneous/CombinationalSum.cpp
@@ -1,29 +1,70 @@
 /*
-Given an array of positive integers arr[] and a sum x, find all unique combinations in arr[] where the sum is equal to x. The same repeated number may be chosen from arr[] unlimited number of times. Elements in a combination (a1, a2, �, ak) must be printed in non-descending order. (ie, a1 <= a2 <= � <= ak).
-The combinations themselves must be sorted in ascending order, i.e., the combination with smallest first element should be printed first. If there is no combination possible the print "Empty" (without quotes).
+Given an array of integers A and a sum B, find all unique combinations in A where the sum is equal to B. The same repeated number may be chosen from A unlimited number of times.
+Note:
+All numbers will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The combinations themselves must be sorted in ascending order.
+If there is no combination possible the print "Empty" (without qoutes).
+Example,
+Given A = 2,4,6,8 and B(sum) = 8,
+A solution set is:
 
-Examples:
-
-Input : arr[] = 2, 4, 6, 8
-x = 8
-Output : [2, 2, 2, 2]
+[2, 2, 2, 2]
 [2, 2, 4]
 [2, 6]
 [4, 4]
 [8]
+
+Input:
+First is T , no of test cases. 1<=T<=500
+Every test case has three lines.
+First line is N, size of array. 1<=N<=12
+Second line contains N space seperated integers(x). 1<=x<=9.
+Third line is the sum B. 1<=B<=30.
+ 
+Output:
+One line per test case, every subset enclosed in () and in every set intergers should be space seperated.(See example)
+
+Example:
+Input:
+3
+4
+7 2 6 5
+16
+11
+6 5 7 1 8 2 9 9 7 7 9
+6
+4
+5 2 2 6
+3
+
+Output:
+(2 2 2 2 2 2 2 2)(2 2 2 2 2 6)(2 2 2 5 5)(2 2 5 7)(2 2 6 6)(2 7 7)(5 5 6)
+(1 1 1 1 1 1)(1 1 1 1 2)(1 1 2 2)(1 5)(2 2 2)(6)
+Empty
 */
+
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
+int flag=0;
 void print(int arr[], int n, vector<int> &v, int &cs, int sum,int index)
 {
 	if (cs == sum)
 	{
+	    flag=1;
 		vector<int>::iterator it;
+		cout<<"(";
 		for (it = v.begin(); it != v.end(); it++)
-			cout << *it << " ";
-		cout << "\n";
+		{
+		    if(it+ 1==v.end())
+		    cout<<*it;
+		    else
+		    cout << *it << " ";
+		}
+			cout<<")";
 		return;
 	}
 	for (int i = index; i < n && cs + arr[i] <= sum; i++)
@@ -35,13 +76,46 @@ void print(int arr[], int n, vector<int> &v, int &cs, int sum,int index)
 		v.pop_back();
 	}
 }
+void removeDuplicates(int arr[],int &n)
+{
+    int index=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(index==-1 || arr[index]!=arr[i])
+        {
+            index++;
+            arr[index]=arr[i];
+        }
+    }
+    n=index+1;
+}
 int main()
 {
-	int arr[] = { 2,4,6,8 };
-	int cs = 0;
-	int s = 8;
-	vector<int> v;
-	print(arr, 4, v, cs, s, 0);
+    int t;
+    cin>>t;
+    int n;
+    int arr[13];
+    int s;
+    int cs;
+    while(t)
+    {
+        cin>>n;
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        sort(arr,arr+n);
+        removeDuplicates(arr,n);
+        cin>>s;
+        vector<int> v;
+        cs=0;
+        flag=0;
+	    print(arr, n, v, cs, s, 0);
+	    if(flag==0)
+	    {
+	        cout<<"Empty";
+	    }
+	    cout<<"\n";
+        t--;
+    }
+	
     return 0;
 }
-
